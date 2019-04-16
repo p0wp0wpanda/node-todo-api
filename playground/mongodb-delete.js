7b0e8d6fe83fb54d90976b47468b34256a181002
Atharva <asphadnis1@gmail.com>
2019-04-17 00:53:01 +0530

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,38 @@
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27019/ToDoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    var db = client.db('ToDoApp');
+
+    //deleteMany
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    
+    
+    //deleteOne
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+
+    //Note :- These statements will work even without the .then() promise call. If we want something(eg: a callback) 
+    //to fire once this statement executes, then we use the .then() statement.
+    //findOneAndDelete
+    db.collection('Todos').findOneAndDelete({ completed: false }).then((result) => {
+        console.log(result);
+    });
+
+
+    //Doesnt work with promises
+    //client.close();
+});
