52eb62a14b5629eb2835d8cf82bd0bb7f23b97a9
Atharva <asphadnis1@gmail.com>
2019-04-17 19:00:28 +0530

    Add GET /todos route and test scripts

--- server/server.js
+++ server/server.js
@@ -21,10 +21,19 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(err);
+    })
+});
+
 app.listen(3030, () => {
     console.log('Server spooled up on port 3030');
 });
 
+module.exports = {app}
 
 // var newTodo = new Todo({
 //     text: 'Cook Dinner'
